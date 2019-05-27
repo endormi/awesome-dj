968af3159c3892e3ed8e16b0ef33f69e5f6ca593
endormi <ernosalo14@gmail.com>
2019-05-27 21:31:29 +0300

    content

--- bs_boilerplate/bs_boilerplate/urls.py
+++ bs_boilerplate/bs_boilerplate/urls.py
@@ -1,4 +1,4 @@
-"""boilerplate URL Configuration
+"""bs_boilerplate URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from starter import views
 
 urlpatterns = [
+    path('', include('starter.urls')),
     path('admin/', admin.site.urls),
 ]
