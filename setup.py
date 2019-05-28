b57ba612acd4086ec3fce3181c86bfa86d0b7760
endormi <ernosalo14@gmail.com>
2019-05-28 09:29:58 +0300

    -rm setup

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="django",
-    description="Reasons why django is awesome for web development",
-    author="endormi (https://github.com/endormi)",
-)
