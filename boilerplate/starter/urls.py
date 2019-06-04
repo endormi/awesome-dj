b7d9686b340ad71450c6dd04ea0a11b56efedf31
endormi <ernosalo14@gmail.com>
2019-06-04 13:24:45 +0300

    about page

--- boilerplate/starter/urls.py
+++ boilerplate/starter/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name="home")
+  path('', views.home, name="home"),
+  path('about/', views.about, name="about")
 ]
