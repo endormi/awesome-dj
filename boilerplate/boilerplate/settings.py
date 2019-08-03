c74ecf2acded1557d8ed9f385cf3fc14cfe8b609
endormi <ernosalo14@gmail.com>
2019-08-03 12:52:23 +0300

    Update base nav dropdown and commented starter folder

--- boilerplate/boilerplate/settings.py
+++ boilerplate/boilerplate/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'starter',
+    'starter', # starter folder
 ]
 
 MIDDLEWARE = [
